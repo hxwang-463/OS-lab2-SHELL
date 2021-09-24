78f4cef945ff19034b633617271e04b80a0a0f6d
HUIXUAN WANG <hw2544@nyu.edu>
2021-09-24 15:09:20 -0400

    simple program like ls -la

--- nyush.c
+++ nyush.c
@@ -10,6 +10,26 @@
 
 void sig_int(int x){}
 
+int my_system(char **argv) {
+    if (fork() == 0) {
+        char* prog_name = (char*)malloc(1001*sizeof(char));
+        if(argv[0][0]==0x2f|argv[0][0]==0x2e)execv(argv[0], argv);
+        char prog_root[1001] = {0};
+        strcpy(prog_root, "/bin/");
+        strcat(prog_root, argv[0]);
+        execv(prog_root, argv);
+        strcpy(prog_root, "/usr/bin/");
+        strcat(prog_root, argv[0]);
+        execv(prog_name, argv);
+        fprintf(stderr, "Error: invalid progarm\n");
+        exit(-1);
+    }
+    else{
+        wait(NULL);
+    }
+    return 0;
+}
+
 void command_line_parser(char* command_line){
 
     char command_line_new[1001];
@@ -17,14 +37,12 @@ void command_line_parser(char* command_line){
     char* command;
     char* rest_command = command_line_new;
     command = strtok_r(rest_command, " ", &rest_command);
-
     if(!strcmp(command, "exit")){
         if(strtok_r(rest_command, " ", &rest_command)){
             fprintf(stderr, "Error: invalid command\n");
             return;
         }
         /* 检查是否有暂停程序
-
 
         */
         int pid = getpid();
@@ -43,15 +61,29 @@ void command_line_parser(char* command_line){
         if(check == -1){
             fprintf(stderr, "Error: invalid directory\n");
             return;
+        return;
         }
     }
+    else if(!strcmp(command, "jobs")){
+
+    }
+    else if(!strcmp(command, "fg")){
+
+    }
 
 
+    char** argv;
+    argv = (char**)malloc(sizeof(char*)*1001);
+    int i = 0;
 
     while (command){
-        // printf("%s\n", command);
+        argv[i]  = (char*)malloc(1001*sizeof(char));
+        strcpy(argv[i], command);
+        i=i+1;
         command = strtok_r(rest_command, " ", &rest_command);
     }
+    argv[i] = NULL;
+    my_system(argv);
 }
 
 int main(){
@@ -66,8 +98,9 @@ int main(){
         current_folder = strrchr(working_directory, slash);
         printf("[nyush %s]$ ", current_folder+1);
         fgets(command_line, 1001, stdin);
+        if(command_line[0] == 0x0A)continue;
         command_line[strlen(command_line)-1] = 0;
-        // printf("%s", command_line);
+        
         command_line_parser(command_line);
 
 
