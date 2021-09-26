4822c73fedf7f4a2c3040979f4296f33a69df749
HUIXUAN WANG <hw2544@nyu.edu>
2021-09-25 20:50:46 -0400

    commit before Huge change on pipe.

--- nyush.c
+++ nyush.c
@@ -10,6 +10,18 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+typedef struct {
+    char **command;
+    int input_fd;
+    char* input_file;
+    int output_fd;
+    char* output_file;
+    struct vari* next;
+}Link;
+Link* head = NULL;
+
+
+
 void sig_int(int x){
     printf("\n");
     return;
@@ -17,8 +29,8 @@ void sig_int(int x){
 
 int my_system(char **argv, int io_redirect, char* redirect_path){
     
-
-    if (fork() == 0) {
+    int pid;
+    if (pid=fork() == 0) {
         if(io_redirect){
             int fp;
             if(io_redirect==1){
@@ -28,7 +40,7 @@ int my_system(char **argv, int io_redirect, char* redirect_path){
                     exit(-1);}
                 dup2(fp, STDIN_FILENO);}
             else{
-                if(io_redirect==2){fp = open(redirect_path, O_RDWR|O_CREAT, 00600);}
+                if(io_redirect==2){fp = open(redirect_path, O_WRONLY|O_CREAT|O_TRUNC, 00600);}
                 else if(io_redirect==3){fp = open(redirect_path, O_RDWR|O_CREAT|O_APPEND, 00600);}
                 else{fprintf(stderr, "Unknown Error 1\n");exit(-1);}
                 if(fp==-1){
@@ -39,7 +51,7 @@ int my_system(char **argv, int io_redirect, char* redirect_path){
         
 
         char* prog_name = (char*)malloc(1001*sizeof(char));
-        if(argv[0][0]==0x2f|argv[0][0]==0x2e){
+        if(argv[0][0]==0x2f||argv[0][0]==0x2e){
             execv(argv[0], argv);
             fprintf(stderr, "Error: invalid progarm\n");
             exit(-1);
@@ -60,7 +72,7 @@ int my_system(char **argv, int io_redirect, char* redirect_path){
     return 0;
 }
 
-void command_line_parser(char* command_line){ //no pipe in this function
+void command_parser(char* command_line, int is_first_command, int is_last_command, int input_fd, int output_fd){ //no pipe in this function
 
     char command_line_new[1001];
     memcpy(command_line_new,command_line, 1001);
@@ -68,6 +80,10 @@ void command_line_parser(char* command_line){ //no pipe in this function
     char* rest_command = command_line_new;
     command = strtok_r(rest_command, " ", &rest_command);
     if(!strcmp(command, "exit")){
+        if(!is_first_command||!is_last_command){
+            fprintf(stderr, "Error: invalid command\n");
+            return;
+        }
         if(strtok_r(rest_command, " ", &rest_command)){
             fprintf(stderr, "Error: invalid command\n");
             return;
@@ -80,6 +96,10 @@ void command_line_parser(char* command_line){ //no pipe in this function
         kill(pid, SIGKILL);
     }
     else if(!strcmp(command, "cd")){
+        if(!is_first_command||!is_last_command){
+            fprintf(stderr, "Error: invalid command\n");
+            return;
+        }
         char *path = strtok_r(rest_command, " ", &rest_command);
         if(strtok_r(rest_command, " ", &rest_command)){
             fprintf(stderr, "Error: invalid command\n");
@@ -96,11 +116,27 @@ void command_line_parser(char* command_line){ //no pipe in this function
         }
     }
     else if(!strcmp(command, "jobs")){
+        if(!is_first_command||!is_last_command){
+            fprintf(stderr, "Error: invalid command\n");
+            return;
+        }
 
+
+
+        return;
     }
     else if(!strcmp(command, "fg")){
+        if(!is_first_command||!is_last_command){
+            fprintf(stderr, "Error: invalid command\n");
+            return;
+        }
 
+
+
+        return;
     }
+
+    Link *one_command=(Link*)malloc(sizeof(Link));
 
 
     char** argv;
@@ -110,17 +146,29 @@ void command_line_parser(char* command_line){ //no pipe in this function
     char redirect_path[1001] = {0};
     int i = 0;
     while (command){
-        if(!strcmp(command, "<")|!strcmp(command, ">")|!strcmp(command, ">>")){
-            if(!strcmp(command, "<"))io_redirect = 1;
-            else if(!strcmp(command, ">"))io_redirect = 2;
-            else io_redirect = 3;    
+        if(!strcmp(command, "<")||!strcmp(command, ">")||!strcmp(command, ">>")){
+            if(!strcmp(command, "<")){
+                io_redirect = 1;
+                if(!is_first_command){
+                    fprintf(stderr, "Error: invalid command\n");
+                    return;}}
+            else if(!strcmp(command, ">")){
+                io_redirect = 2;
+                if(!is_last_command){
+                    fprintf(stderr, "Error: invalid command\n");
+                    return;}}
+            else{
+                io_redirect = 3;    
+                if(!is_last_command){
+                    fprintf(stderr, "Error: invalid command\n");
+                    return;}}
             command = strtok_r(rest_command, " ", &rest_command);
             if(command == NULL){
                 fprintf(stderr, "Error: invalid command\n");
                 return;}
             strcpy(redirect_path, command);
             command = strtok_r(rest_command, " ", &rest_command);
-            if(command != NULL){
+            if(command != NULL){ //
                 fprintf(stderr, "Error: invalid command\n");
                 return;}
             argv[i+1] = NULL;
@@ -133,8 +181,48 @@ void command_line_parser(char* command_line){ //no pipe in this function
         command = strtok_r(rest_command, " ", &rest_command);
     }
     argv[i] = NULL;
+
+    one_command->command = (char**)malloc(sizeof(argv));
+    memcpy(one_command->command, argv, sizeof(argv));
+
     my_system(argv, 0, NULL);
 }
+
+
+void command_line_parser(char* command_line){  //deal with pipe
+    char command_line_new[1001];
+    memcpy(command_line_new, command_line, 1001);
+    char* command_prev = NULL; 
+    char* command; 
+    char* command_next; 
+    char* rest_command = command_line_new;
+    int is_first_command, is_last_command;
+    command = strtok_r(rest_command, "|", &rest_command);
+    int pipefd[2];
+    int input_fd, output_fd;
+    while (command){
+        command_next = strtok_r(rest_command,  "|", &rest_command);
+        if(command_prev==NULL) is_first_command = 1; else is_first_command = 0;
+        if(command_next==NULL) is_last_command = 1; else is_last_command = 0;
+        if(command_prev){ //input from previous
+            input_fd = pipefd[0];
+        }
+        else input_fd = -1;
+        if(command_next){ //output to next
+            pipe(pipefd);
+            output_fd = pipefd[1];
+        }
+        else output_fd = -1;
+        
+        if(command[0]==0x20)command = command+1;
+        command_parser(command, is_first_command, is_last_command, input_fd, output_fd);
+        command_prev=command;
+        command=command_next;
+    }
+
+
+}
+
 
 int main(){
     signal(SIGINT, sig_int);
@@ -155,7 +243,6 @@ int main(){
         fgets(command_line, 1001, stdin);
         if(command_line[0] == 0x0A)continue;
         command_line[strlen(command_line)-1] = 0;
-        
         command_line_parser(command_line);
 
 
